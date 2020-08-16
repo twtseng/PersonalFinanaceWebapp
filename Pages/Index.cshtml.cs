aa1747cc6d4d42516cafad1787d1a8928a4afb85
Tom Tseng <tomwstseng@gmail.com>
2020-08-15 18:22:56 -0700

    debugging

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace PersonalFinanceWebapp.Pages
 {
@@ -12,14 +13,16 @@ namespace PersonalFinanceWebapp.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            Configuration = configuration;
         }
-
+        public IConfiguration Configuration { get; }
         public void OnGet()
         {
-
+                _logger.LogInformation($"Authentication:Facebook:AppId: [{Configuration["Authentication:Facebook:AppId"]}]");
+                _logger.LogInformation($"Authentication:Facebook:AppSecret: [{Configuration["Authentication:Facebook:AppSecret"]}]");
         }
     }
 }
