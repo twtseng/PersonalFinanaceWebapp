578af0101488884730ced0fae5c33745d54af18d
twtseng <twtseng@users.noreply.github.com>
2020-12-06 19:52:51 -0800

    Filter on last 2 months and last year

--- ClientApp/src/components/PivotView.js
+++ ClientApp/src/components/PivotView.js
@@ -8,12 +8,31 @@ import Pivot from './Pivot';
 */
 const PivotView = () => {
     const [data, setData] = React.useState([]);
-    const [last2MonthsData, setLast2MonthsData] = React.useState([]);
+    const [last2MonthFilter, setLast2MonthFilter] = React.useState({});
+    const [lastYearFilter, setLastYearFilter] = React.useState({});
     const [filters, setFilters] = React.useState([]);
     const { expenseData } = React.useContext(AppContext);
     const setExpenseData = (data) => {
         setData(data);
-        // TODO: Set the filters for the week and monthly 
+        // Set the filters for the week and monthly
+        const monthHash = {};
+        data.forEach(x => monthHash[x.monthId] = x.monthId);
+        const months = Object.keys(monthHash);
+        let twoMonthFilter = {};
+        let oneYearFilter = {};
+        for(let i = 0; i < months.length; ++i) {
+            if (i < months.length - 2) {
+                twoMonthFilter[months[i]] = true;
+            }
+            if (i < months.length - 12) {
+                oneYearFilter[months[i]] = true;
+            }
+        }
+        setLast2MonthFilter(twoMonthFilter);
+        setLastYearFilter(oneYearFilter);
+        console.log(JSON.stringify(data));
+        console.log(`twoMonthFilter: ${JSON.stringify(twoMonthFilter)}`);
+        console.log(`oneYearFilter: ${JSON.stringify(oneYearFilter)}`);
     }
     React.useEffect(() => {
         expenseData.getExpensesData(setExpenseData);
@@ -21,9 +40,24 @@ const PivotView = () => {
 
     return (
         <div>
-            <Pivot title="Weekie" pivotprops={{aggregatorName:"Sum", vals:["amount"], cols:["year","month","weeknum"], rows:["category"]}} />
-            <Pivot title="Month long summary" pivotprops={{aggregatorName:"Sum", vals:["amount"], cols:["year","month"], rows:["category"]}} />
-            <Pivot title="Year long summary" pivotprops={{aggregatorName:"Sum", vals:["amount"], cols:["year"], rows:["category"]}} />
+            <Pivot title="Past 2 months" pivotprops={{
+                aggregatorName:"Sum", 
+                vals:["amount"], 
+                cols:["year","month","weekNum"], 
+                rows:["category"],
+                valueFilter:{"monthId":last2MonthFilter}
+                }} />
+            <Pivot title="Past year" pivotprops={{
+                aggregatorName:"Sum", 
+                vals:["amount"], 
+                cols:["year","month"], 
+                rows:["category"], 
+                valueFilter:{"monthId":lastYearFilter}}} />
+            <Pivot title="Year long summary" pivotprops={{
+                aggregatorName:"Sum", 
+                vals:["amount"], 
+                cols:["year"], 
+                rows:["category"]}} />
            
         </div>
     )
